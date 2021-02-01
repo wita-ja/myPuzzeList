14f31af6979fe6e4ccf229ce6f1dfbca0535324e
AvocadoGag <45439740+AvocadoGag@users.noreply.github.com>
2021-02-01 19:02:54 +0200

    initial BE

---
+++ Backend/src/main/java/com/Vitalij/myPuzzleList/MyPuzzleListApplication.java
@@ -0,0 +1,13 @@
+package com.Vitalij.myPuzzleList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPuzzleListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPuzzleListApplication.class, args);
+	}
+
+}
