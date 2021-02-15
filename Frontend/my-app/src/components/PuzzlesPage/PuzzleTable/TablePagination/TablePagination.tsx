f0759778a37679bf994875bc6157c87d738a3308
wita <witalik1212@mail.ru>
2021-02-15 22:32:39 +0200

    table pagination

--- Frontend/my-app/src/components/PuzzlesPage/PuzzleTable/TablePagination/TablePagination.tsx
+++ Frontend/my-app/src/components/PuzzlesPage/PuzzleTable/TablePagination/TablePagination.tsx
@@ -1,25 +1,71 @@
-import React from 'react';
-import { Pagination, Icon } from 'semantic-ui-react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Pagination, Icon, PaginationProps } from 'semantic-ui-react';
 
 interface TablePagination {
   defaultActivePage: number;
   totalPages: number;
+  currentPage?: number; //TODO check if needed later
 }
 
 const TablePagination = (props: TablePagination) => {
+  const { totalPages, defaultActivePage, currentPage } = props;
+  const [state, setState] = useState(props);
+
+  useEffect(() => {
+    currentPage
+      ? setState({ ...state, currentPage: currentPage })
+      : setState({ ...state, currentPage: defaultActivePage });
+  }, []);
+
+  const handlePaginationChange = (
+    e: React.MouseEvent<HTMLAnchorElement>,
+    pageInfo: PaginationProps
+  ) => {
+    /* setState({
+      ...state,
+      currentPage: pageInfo.activePage as number,
+    }); */
+    setState((prevState: TablePagination) => {
+      return {
+        ...prevState,
+        currentPage: pageInfo.activePage,
+      } as TablePagination;
+    });
+    console.log('state after set: ' + state.currentPage);
+  };
+
   return (
-    <Pagination
-      defaultActivePage={props.defaultActivePage}
-      ellipsisItem={{
-        content: <Icon name='ellipsis horizontal' />,
-        icon: true,
-      }}
-      firstItem={{ content: <Icon name='angle double left' />, icon: true }}
-      lastItem={{ content: <Icon name='angle double right' />, icon: true }}
-      prevItem={{ content: <Icon name='angle left' />, icon: true }}
-      nextItem={{ content: <Icon name='angle right' />, icon: true }}
-      totalPages={props.totalPages}
-    />
+    <>
+      {console.log('state before link set: ' + state.currentPage)}
+      <Link to={`puzzles/${state.currentPage}`}>
+        <Pagination
+          defaultActivePage={defaultActivePage}
+          ellipsisItem={{
+            content: <Icon name='ellipsis horizontal' />,
+            icon: true,
+          }}
+          firstItem={{
+            content: <Icon name='angle double left' />,
+            icon: true,
+          }}
+          lastItem={{
+            content: <Icon name='angle double right' />,
+            icon: true,
+          }}
+          prevItem={{
+            content: <Icon name='angle left' />,
+            icon: true,
+          }}
+          nextItem={{
+            content: <Icon name='angle right' />,
+            icon: true,
+          }}
+          totalPages={totalPages}
+          onPageChange={handlePaginationChange}
+        />
+      </Link>
+    </>
   );
 };
 
